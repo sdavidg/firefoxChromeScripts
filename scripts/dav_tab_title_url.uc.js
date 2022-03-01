34d774fc496fc38d3f4b0f68d6882493c3b1b753
David <sdavidgomez@gmail.com>
2022-03-01 17:53:37 +0100

    add Preview

--- scripts/dav_tab_title_url.uc.js
+++ scripts/dav_tab_title_url.uc.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name                 dav_tab_title_url.uc
-// @version              2.0
-// @description          dav_tab_title_url.uc
+// @name				 dav_tab_title_url.uc
+// @version				 3.0
+// @description			 dav_tab_title_url.uc
 // ==/UserScript==
 
 /*
@@ -12,286 +12,300 @@ https://github.com/LouCypher/tab-tooltip-url
 (function(){
 	if(location.href != 'chrome://browser/content/browser.xhtml') return;
 
-	var linkifiesUrl = true;
+	var linkifiesUrl = true,
+		showPreview = true,
+		timeUpdatePreview = 1000, //miliseconds
+		idTimeoutUpdatePreview
+	;
+	var widthCanvasIMG = 400;
+	var heightCanvasIMG = widthCanvasIMG*9/16;
 
-function linkifiesLocationBar(url)
-{
-	const colorizeExtensionFile = false;
-	const CLIKS = {
-		left: 0,
-		middle: 1,
-		right: 2
-	}
-
-	var styleString = (style) => {
-		return Object.keys(style).reduce((prev, curr) => {
-			return `${prev += curr.split(/(?=[A-Z])/).join('-').toLowerCase()}:${style[curr]};`
-		}, '');
-	};
-
-	function createElement(elto) {
-		elto = Object.assign({
-			attr: {},
-			evtListener: [],
-			estilos: {}
-		}, elto);
-		var node = document.createElement(elto.type);
-
-		Object.keys(elto.attrArray).forEach(key => {
-			if(key == "innerHTML"){
-				node.innerHTML = encodeHTML(elto.attrArray[key]);
-			}
-			else {
-				node.setAttribute(key, elto.attrArray[key]);
-			}
-		});
-
-		elto.evtListener.forEach(evt => {
-			node.addEventListener(evt.type, evt.funcion, false);
-		});
-
-		let estilo = styleString(elto.estilos);
-		if (estilo) {
-			node.setAttribute("style", estilo);
+	function linkifiesLocationBar(url){
+		const colorizeExtensionFile = false;
+		const CLIKS = {
+			left: 0,
+			middle: 1,
+			right: 2
 		}
 
-		return node;
-	}
+		var styleString = (style) => {
+			return Object.keys(style).reduce((prev, curr) => {
+				return `${prev += curr.split(/(?=[A-Z])/).join('-').toLowerCase()}:${style[curr]};`
+			}, '');
+		};
 
-	function appendPart(text, clase) {
-		if (!text) return;
+		function createElement(elto) {
+			elto = Object.assign({
+				attr: {},
+				evtListener: [],
+				estilos: {}
+			}, elto);
+			var node = document.createElement(elto.type);
 
-		if(clase == "pathname")
-		{
+			Object.keys(elto.attrArray).forEach(key => {
+				if(key == "innerHTML"){
+					node.innerHTML = encodeHTML(elto.attrArray[key]);
+				}
+				else {
+					node.setAttribute(key, elto.attrArray[key]);
+				}
+			});
+
+			elto.evtListener.forEach(evt => {
+				node.addEventListener(evt.type, evt.funcion, false);
+			});
+
+			let estilo = styleString(elto.estilos);
+			if (estilo) {
+				node.setAttribute("style", estilo);
+			}
+
+			return node;
+		}
+
+		function appendPart(text, clase) {
+			if (!text) return;
+
+			if(clase == "pathname")
+			{
+				let sp = createElement({
+					type: "label",
+					attrArray: {
+						class: "label_pathname",
+						innerHTML:"/"
+					}
+				});
+				divLocationBar.appendChild(sp);
+			}
 			let sp = createElement({
-				type: "label",
+				type: "span",
 				attrArray: {
-					class: "label_pathname",
-					innerHTML:"/"
+					class: clase,
+					innerHTML:text
 				}
 			});
 			divLocationBar.appendChild(sp);
+			sp.setAttribute("href", divLocationBar.textContent);
+			return sp;
 		}
-		let sp = createElement({
-			type: "span",
+
+		var divLocationBar = createElement({
+			type: "div",
 			attrArray: {
-				class: clase,
-				innerHTML:text
+				class: "claseLocationBarTooltip"
 			}
 		});
-		divLocationBar.appendChild(sp);
-		sp.setAttribute("href", divLocationBar.textContent);
-		return sp;
-	}
 
-	function clickPart(evt) {
-		if (evt.button == CLIKS.right) return;
-
-		let target = evt.target;
-		if (target.className != "protocol") {
-			let href = target.getAttribute("href");
-			console.log("href", href);
-			evt.stopPropagation();
+		try {
+			var { protocol, hostname, port, pathname, hash, search } = new URL(url);
+		} catch (e) {
+			divLocationBar.innerHTML = encodeHTML(url);
+			return divLocationBar;
 		}
-	}
 
-	var divLocationBar = createElement({
-		type: "div",
-		attrArray: {
-			class: "claseLocationBarTooltip"
+		var partido = hostname.split(".");
+		var subdomain;
+		if(partido.length > 2 && !partido.every( v => v == v-0 ))//chequeamos que no sean todos numeros, porque entonces es una IP
+		{
+			subdomain = partido.splice(0, partido.length-2).join(".");
+			hostname= partido.join(".");
 		}
-	});
 
-	try {
-		var { protocol, hostname, port, pathname, hash, search } = new URL(url);
-	} catch (e) {
-		divLocationBar.innerHTML = encodeHTML(url);
+		appendPart(protocol + "//", "protocol");
+		if (subdomain) {
+			appendPart(subdomain+".", "subdomain");
+		}
+		appendPart(hostname, "hostname");
+		if (port) {
+			appendPart(":" + port, "port");
+		}
+		var arrayPathname = pathname.split("/");
+		var arrayPathnameLength = arrayPathname.length;
+		arrayPathname.forEach((elto, index) => {
+			if (elto) {
+				let sp = appendPart(elto, "pathname");
+				if(colorizeExtensionFile && index == arrayPathnameLength-1)
+				{
+					let arrayDot = elto.split(".");
+					if(arrayDot.length > 1)
+					{
+						let extension = arrayDot.pop();
+						sp.innerHTML = "";
+						sp.appendChild(createElement({
+							type: "locationBarTag",
+							attrArray: {
+								href:sp.getAttribute("href"),
+								innerHTML: arrayDot.join(".")
+							}
+						}));
+						sp.appendChild(createElement({
+							type: "locationBarTag",
+							attrArray: {
+								class: "extension",
+								href:sp.getAttribute("href"),
+								innerHTML: "."+extension
+							}
+						}));
+					}
+				}
+			}
+		});
+		appendPart(search, "search");
+		appendPart(hash, "hash");
+
 		return divLocationBar;
 	}
 
-	var partido = hostname.split(".");
-	var subdomain;
-	if(partido.length > 2 && !partido.every( v => v == v-0 ))//chequeamos que no sean todos numeros, porque entonces es una IP
-	{
-		subdomain = partido.splice(0, partido.length-2).join(".");
-		hostname= partido.join(".");
-	}
-
-	appendPart(protocol + "//", "protocol");
-	if (subdomain) {
-		appendPart(subdomain+".", "subdomain");
-	}
-	appendPart(hostname, "hostname");
-	if (port) {
-		appendPart(":" + port, "port");
-	}
-	var arrayPathname = pathname.split("/");
-	var arrayPathnameLength = arrayPathname.length;
-	arrayPathname.forEach((elto, index) => {
-		if (elto) {
-			let sp = appendPart(elto, "pathname");
-			if(colorizeExtensionFile && index == arrayPathnameLength-1)
+	var CSS_Loader = {
+		type:{
+			AGENT_SHEET: 0,
+			USER_SHEET: 1,
+			AUTHOR_SHEET: 2
+		},
+		type_default:1,
+		sss: Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService),
+		load: function(cssCode) {
+			this.unload(cssCode);
+			var uri = Services.io.newURI("data:text/css;charset=utf-8," + encodeURIComponent(cssCode), null, null);
+			this.sss.loadAndRegisterSheet(uri, this.type_default);
+		},
+		unload: function(cssCode) {
+			var uri = Services.io.newURI("data:text/css;charset=utf-8," + encodeURIComponent(cssCode), null, null);
+			if (this.sss.sheetRegistered(uri,this.type_default))
 			{
-				let arrayDot = elto.split(".");
-				if(arrayDot.length > 1)
-				{
-					let extension = arrayDot.pop();
-					sp.innerHTML = "";
-					sp.appendChild(createElement({
-						type: "locationBarTag",
-						attrArray: {
-							href:sp.getAttribute("href"),
-							innerHTML: arrayDot.join(".")
-						}
-					}));
-					sp.appendChild(createElement({
-						type: "locationBarTag",
-						attrArray: {
-							class: "extension",
-							href:sp.getAttribute("href"),
-							innerHTML: "."+extension
-						}
-					}));
-				}
+				this.sss.unregisterSheet(uri,this.type_default);
 			}
 		}
-	});
-	appendPart(search, "search");
-	appendPart(hash, "hash");
-
-	return divLocationBar;
-}
-
-
-var CSS_Loader = {
-	type:{
-		AGENT_SHEET: 0,
-		USER_SHEET: 1,
-		AUTHOR_SHEET: 2
-	},
-	type_default:1,
-	sss: Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService),
-	load: function(cssCode) {
-		this.unload(cssCode);
-		var uri = Services.io.newURI("data:text/css;charset=utf-8," + encodeURIComponent(cssCode), null, null);
-		this.sss.loadAndRegisterSheet(uri, this.type_default);
-	},
-	unload: function(cssCode) {
-		var uri = Services.io.newURI("data:text/css;charset=utf-8," + encodeURIComponent(cssCode), null, null);
-		if (this.sss.sheetRegistered(uri,this.type_default))
-		{
-			this.sss.unregisterSheet(uri,this.type_default);
-		}
 	}
-}
-function asignaEstilos(){
-	var style = `
-	/*************************************
-	*************** COLORS ***************
-	*************************************/
-	.claseLocationBarTooltip{
-	  font-family:monospace;
-	  margin:5px;
-	}
-	.claseLocationBarTooltip .label_pathname{
-		color: black;
-	}
-	.claseLocationBarTooltip span.protocol{
-		font-weight: normal;
-		color: #777777;
-		margin-right: -1px;
-	}
-	.claseLocationBarTooltip .subdomain {
-		font-weight: bold;
-		color: #C68007;
-	}
-	.claseLocationBarTooltip span.hostname{
-		font-weight: bold;
-		color: red;
-	}
-	.claseLocationBarTooltip span.port{
-		color: #5F58A3;
-	}
-	.claseLocationBarTooltip span.pathname{
-		color: black;
-	}
-	.claseLocationBarTooltip span.hash{
-		color: #1054C9;
-		margin-left: -1px;
-	}
-	.claseLocationBarTooltip span.search{
-		color: #03AA03;
-		margin-left: -1px;
-	}
-	.claseLocationBarTooltip .extension{
-		color: rgb(96,86,143);
-	}
-
-	/*************************************
-	*************** Flecha pathname ***************
-	************************************
-	.claseLocationBarTooltip .pathname:after{
-		content:' ';
-		display: block;
-		position: absolute;
-		border-style: solid;
-		border-width: 4px 4px 4px 7px;
-		border-color: transparent transparent transparent #6fa880;
-		top: 3px;
-		left: 0px;
-		width: 0px;
-		height: 0px;
-	}
-	.claseLocationBarTooltip span.pathname{
-		padding-left:9px;
-		margin: 0 2px;
-	}
-	.claseLocationBarTooltip,
-	.claseLocationBarTooltip span{
-		position: relative;
-		display: inline-block;
-	}
-	.claseLocationBarTooltip .label_pathname{
-		display: none;
-	}
-	*/
-    `;
-	CSS_Loader.load(style);
-}
-
-	var style = `
-		#dav_tab_title_url {
-			font-family: monospace;
-		}
-		.divTitle{
-			display: block;
-		}
-		#dav_tab_title_url .title,
-		#dav_tab_title_url .url,
-		#dav_tab_title_url .reloadDiv {
+	function asignaEstilosLinkifiesUrl(){
+		var style = `
+		/*************************************
+		*************** COLORS ***************
+		*************************************/
+		.claseLocationBarTooltip{
+			font-family:monospace;
 			margin:5px;
 		}
-		#dav_tab_title_url .title,
-		#dav_tab_title_url .url{
-			display:inline-block;
+		.claseLocationBarTooltip .label_pathname{
+			color: black;
 		}
-		#dav_tab_title_url .title{
-			color:black;
+		.claseLocationBarTooltip span.protocol{
+			font-weight: normal;
+			color: #777777;
+			margin-right: -1px;
+		}
+		.claseLocationBarTooltip .subdomain {
 			font-weight: bold;
+			color: #C68007;
 		}
-		#dav_tab_title_url .url{
-			color:blue;
-		}
-		#dav_tab_title_url .reloadText{
-			color: teal;
-		}
-		#dav_tab_title_url .reloadValue{
+		.claseLocationBarTooltip span.hostname{
+			font-weight: bold;
 			color: red;
-			font-weight: bold;
-			margin:0 3px;
 		}
-	`;
-	CSS_Loader.load(style);
+		.claseLocationBarTooltip span.port{
+			color: #5F58A3;
+		}
+		.claseLocationBarTooltip span.pathname{
+			color: black;
+		}
+		.claseLocationBarTooltip span.hash{
+			color: #1054C9;
+			margin-left: -1px;
+		}
+		.claseLocationBarTooltip span.search{
+			color: #03AA03;
+			margin-left: -1px;
+		}
+		.claseLocationBarTooltip .extension{
+			color: rgb(96,86,143);
+		}
+
+		/*************************************
+		*************** Flecha pathname ***************
+		************************************
+		.claseLocationBarTooltip .pathname:after{
+			content:' ';
+			display: block;
+			position: absolute;
+			border-style: solid;
+			border-width: 4px 4px 4px 7px;
+			border-color: transparent transparent transparent #6fa880;
+			top: 3px;
+			left: 0px;
+			width: 0px;
+			height: 0px;
+		}
+		.claseLocationBarTooltip span.pathname{
+			padding-left:9px;
+			margin: 0 2px;
+		}
+		.claseLocationBarTooltip,
+		.claseLocationBarTooltip span{
+			position: relative;
+			display: inline-block;
+		}
+		.claseLocationBarTooltip .label_pathname{
+			display: none;
+		}
+		*/
+		`;
+		CSS_Loader.load(style);
+	}
+	function asignaEstilos(){
+		var style = `
+			#dav_tab_title_url {
+				font-family: monospace;
+			}
+			.divTitle{
+				display: block;
+			}
+			#dav_tab_title_url .title,
+			#dav_tab_title_url .url,
+			#dav_tab_title_url .reloadDiv {
+				margin:5px;
+			}
+			#dav_tab_title_url .title,
+			#dav_tab_title_url .url{
+				display:inline-block;
+			}
+			#dav_tab_title_url .title{
+				color:black;
+				font-weight: bold;
+			}
+			#dav_tab_title_url .url{
+				color:blue;
+			}
+			#dav_tab_title_url .reloadText{
+				color: teal;
+			}
+			#dav_tab_title_url .reloadValue{
+				color: red;
+				font-weight: bold;
+				margin:0 3px;
+			}
+		`;
+		CSS_Loader.load(style);
+	}
+	function asignaEstilosPreview(){
+
+		var style = `
+			#dav_tab_title_url[hidePreview] #imgPreview
+			{
+				display: none;
+			}
+			#dav_tab_title_url img#imgPreview{
+				border: 1px solid #27a9e7;
+				border-radius:2px;
+				width: `+widthCanvasIMG+`px;
+				height: `+heightCanvasIMG+`px;
+				max-width: `+widthCanvasIMG+`px;
+				max-height: `+heightCanvasIMG+`px;
+				min-width: `+widthCanvasIMG+`px;
+				min-height: `+heightCanvasIMG+`px;
+			}
+		`;
+		CSS_Loader.load(style);
+	}
 
 	function paddy(n, p, c) {
 		var pad_char = typeof c !== 'undefined' ? c : '0';
@@ -449,15 +463,73 @@ function asignaEstilos(){
 
 		return df;
 	}
+
 	function showing(tip, target){
 		var tab = getTabElto(target);
 		if(!tab) return false;
 		tip.textContent = "";
 		tip.appendChild(getTooltipData(tab));
+		if(showPreview){
+			let img = createIMG();
+
+			let divImg = createElement({type:"div", attr:{class:"divImg"}})
+			divImg.appendChild(img);
+			tip.appendChild(divImg);
+			getTooltipIMG(img, tab, tip);
+		}
 		return true;
 	}
 	function hidding(tip, target){
+		if(idTimeoutUpdatePreview){
+			clearTimeout(idTimeoutUpdatePreview);
+		}
+	}
+	function createIMG(){
+		let htmlns = "http://www.w3.org/1999/xhtml";
+		let img = document.createElementNS(htmlns, "img");
+		img.setAttribute("id", "imgPreview");
+		return img;
+	}
+	function getTooltipIMG(img, tab, tip){
+		var browser = tab.linkedBrowser;
+		let pending = tab.hasAttribute("pending") || !browser.browsingContext;
+		let docURI = pending ? null : browser?.documentURI || browser?.currentURI;
 
+		let url = docURI?.spec;
+		let isBlank = !url || url === "about:blank";
+		if (isBlank || pending) {
+			tip.setAttribute("hidePreview", "true");
+			return;
+		}
+
+		var canvas = PageThumbs.createCanvas(window);
+		PageThumbs.captureToCanvas(
+			browser,
+			canvas,
+			{
+				fullScale:true,
+				isImage:true,
+				targetWidth: widthCanvasIMG,
+				fullViewport: true,
+			},
+			true
+		).then(x=>{
+			canvas.toBlob(blob => {
+				var reader = new FileReader();
+				reader.readAsDataURL(blob);
+				reader.onload = function() {
+				  var base64data = reader.result;
+				  img.setAttribute("src", base64data);
+				  tip.removeAttribute("hidePreview");
+				}
+			});
+		});
+		//idTimeoutUpdatePreview = setTimeout(getTooltipIMG, timeUpdatePreview, img, tab, tip);
+
+		idTimeoutUpdatePreview = setTimeout(
+			() => requestIdleCallback(() => requestAnimationFrame(() => getTooltipIMG(img, tab, tip))),
+			timeUpdatePreview
+		);
 	}
 
 	var tipId = "dav_tab_title_url"
@@ -478,7 +550,11 @@ function asignaEstilos(){
 	arrowscrollbox.setAttribute("popupsinherittooltip", "true");
 	document.getElementById("mainPopupSet").appendChild(tip);
 	tip.textContent = "";
+	asignaEstilos();
+	if(showPreview){
+		asignaEstilosPreview();
+	}
 	if(linkifiesUrl){
-		asignaEstilos();
+		asignaEstilosLinkifiesUrl();
 	}
 })();
